4f9b38eb344570ec80ea40629fb0f495fe9e4476
Laura <lmagallanes@ces.com.uy>
2026-01-23 10:58:23 -0300

    primer commit

---
+++ src/principal/Principal.java
@@ -0,0 +1,16 @@
+package principal;
+
+import java.util.Arrays;
+
+public class Principal {
+    public static void main(String[] args) {
+        // Primer linea escrita de código
+        System.out.println("Hola mundo!");
+        System.out.println("Nuestro nuevo codigo");
+
+        System.out.print("Esto ");
+        System.out.print("Es una prueba de impresión");
+
+
+    }
+}
